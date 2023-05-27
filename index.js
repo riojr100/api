24872d8832630df0676f731b0191c6d45617baf0
riojr100 <riojr100@gmail.com>
2023-05-27 13:17:43 +0700

    yes

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const axios = require("axios");
 // Initialize Express
 const app = express();
 
-app.get("/:id", (req, res) => {
+app.get("/manga/:id", (req, res) => {
   const id = req.params.id.split(",");
+  axios
+    .get(`https://api.jikan.moe/v4/manga/${id}`)
+    .then((res) => console.log(res))
+    .finally((res) => console.log(res));
   res.send(`id : ${id}`);
 });
 
