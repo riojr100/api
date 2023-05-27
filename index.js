6f540c5d087629933001dc37b4dbcbe607e0c0fa
riojr100 <riojr100@gmail.com>
2023-05-27 17:58:55 +0700

    yes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 // Initialize Express
 const app = express();
-import cors from "cors";
+const cors = require("cors");
 
 app.use(
   cors({
