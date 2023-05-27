bb23a5e54af1612ec05374e1841a74d3575c992f
riojr100 <riojr100@gmail.com>
2023-05-27 15:47:49 +0700

    yes

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ app.get("/manga/searchMangaById/:id", (request, response) => {
 
 app.get("/manga/searchManga/:query", (request, response) => {
   const query = request.params.query.split(",");
-  axios.get(`https://api.jikan.moe/v4/manga/?q=${query}`).then((res) => {
-    console.log(res.data.mal_id);
+  axios.get(`https://api.jikan.moe/v4/manga/?q=${query}&page=1`).then((res) => {
     response.send(res.data);
   });
+});
+
+app.get("/manga/recommendation", (request, response) => {
+  axios
+    .get("https://api.jikan.moe/v4/recommendations/manga?page=1")
+    .then((res) => {
+      response.send(res.data);
+    });
 });
 
 app.listen(5000, () => {
