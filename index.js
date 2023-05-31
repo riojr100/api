e2ac4319e2c99d027ec434fd5a9ac95f347bcb90
riojr100 <riojr100@gmail.com>
2023-05-31 09:10:45 +0700

    yes

--- index.js
+++ index.js
@@ -17,11 +17,18 @@ app.get("/manga/searchMangaById/:id", (request, response) => {
   });
 });
 // manga/searchManga/{string}
+// working filter V
+//https://api.jikan.moe/v4/manga?page=1&limit=20&q=oshi%20no%20ko&type=manga&min_score=0&max_score=10&status=&sfw=true
 app.get("/manga/searchManga/:query", (request, response) => {
   const query = request.params.query.split(",");
-  axios.get(`https://api.jikan.moe/v4/manga/?q=${query}&page=1`).then((res) => {
-    response.send(res.data);
-  });
+  // axios.get(`https://api.jikan.moe/v4/manga/?q=${query}&page=1`).then((res) => {
+  axios
+    .get(
+      `https://api.jikan.moe/v4/manga?page=1&limit=20&q=${query}&type=manga&min_score=0&max_score=10&status=&sfw=true`
+    )
+    .then((res) => {
+      response.send(res.data);
+    });
 });
 // /manga/recommendation
 app.get("/manga/recommendation", (request, response) => {
